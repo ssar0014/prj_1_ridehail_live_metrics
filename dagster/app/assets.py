a5d1195a341da59c9984e495f67149a4ca3d32e8
ssar0014 <subhasish.sarkar93@gmail.com>
2025-02-21 17:58:40 +1100

    testing git credential helper

--- dagster/app/assets.py
+++ dagster/app/assets.py
@@ -13,7 +13,7 @@ def processed_data():
     data_dictionary = json.dumps(data_dictionary, indent=4)
     with open("data/result.json", "w") as fp:
         json.dump(data_dictionary, fp)
-    return f"file processing complete!"
+    return f"{idx} file processing complete!"
 
 
 defs = dg.Definitions(assets=[processed_data])
