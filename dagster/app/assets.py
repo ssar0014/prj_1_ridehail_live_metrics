3cf7141cd4da8e7c7f44c9a447a4fafa91b43a83
Subhasish Sarkar <subhasish.sarkar@connexionltd.com>
2025-02-21 17:26:09 +1100

    added a sample dagster deployment, will improve on this

---
+++ dagster/app/assets.py
@@ -0,0 +1,19 @@
+import dagster as dg
+import csv
+import json
+
+
+@dg.asset
+def processed_data():
+    data_dictionary = {}
+    with open("data/sample_data.csv", newline="") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=" ", quotechar="|")
+        for idx, row in enumerate(spamreader):
+            data_dictionary[idx] = row
+    data_dictionary = json.dumps(data_dictionary, indent=4)
+    with open("data/result.json", "w") as fp:
+        json.dump(data_dictionary, fp)
+    return "file processing complete!"
+
+
+defs = dg.Definitions(assets=[processed_data])
